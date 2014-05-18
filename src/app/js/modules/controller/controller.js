ac5c7199c79c8b5be1dcb48f5b3a0a64c3af9179
Juan Carlos Picado <juan@encuestame.org>
2014-05-18 14:23:44 +0200

    Removed old site

--- src/app/js/modules/controller/controller.js
+++
@@ -1,9 +0,0 @@
-/*global define:false, module:false*/
-
-'use strict';
-
-define(['marionette'],function(marionette){
-  var MyApp = new Marionette.Application();
-  // do something with depA
-  return MyApp;
-});
